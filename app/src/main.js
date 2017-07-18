36f16068c8ebbbdf1ea01cb86bd6c8fd23649522
Daniel T. Lee <danieltimlee@gmail.com>
2017-07-18 19:50:01 +0900

    Vue Instance Init

---
+++ app/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './App'
+
+new Vue({
+  el: '#app', // <index.html> Selector will be replaced by template.
+  template: '<App/>', // String template or Object App template.
+  components: {App} // List of Components
+});
